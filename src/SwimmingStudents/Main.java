8e019eb676b385405f18c037a004519cdea649a5
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2020-12-31 06:02:59 +0000

    Editing main folder and setting up some basic things

---
+++ src/SwimmingStudents/Main.java
@@ -0,0 +1,21 @@
+package SwimmingStudents;
+
+public class main {
+
+  private People Student;
+  private People Teacher;
+  private People Supervisor;
+
+  public void setStudent(People people){
+    this.Student = people;
+  }
+
+  public void setStudent(People people){
+    this.Teacher = people;
+  }
+
+  public void setStudent(People people){
+    this.Supervisor = people;
+  }
+  
+}
