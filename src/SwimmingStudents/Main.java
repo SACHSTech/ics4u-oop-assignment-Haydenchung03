49e0669f9ebe3f7f3f272f398b7d687201e024d3
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2021-01-06 02:30:12 +0000

    editing main

--- src/SwimmingStudents/Main.java
+++ src/SwimmingStudents/Main.java
@@ -5,7 +5,8 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) throws IOEXception {
-
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    
     int intStudent;
     int intTeacher;
     int intSupervisor;
@@ -16,6 +17,22 @@ public class Main {
     int intLevel;
     int intLane;
     int intAge;
+
+    String strOption;
+
+    System.out.println("Hi, welcome to swimming pool online orginizer");
+    System.out.println("Are you a: student, teacher, supervisor or parent");
+    strOption = keyboard.readLine();
+    if(strOption.equalsIgnoreCase("student")) {
+
+    }else if(strOption.equalsIgnoreCase("teacher")) {
+
+    }else if(strOption.equalsIgnoreCase("supervisor")) {
+
+    }else if(strOption.equalsIgnoreCase("parent")) {
+      
+    }
+
     
   }
  
