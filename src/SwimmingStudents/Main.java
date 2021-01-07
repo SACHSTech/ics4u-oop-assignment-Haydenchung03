64a75ee8b09c6433b11a84bc48c678dc0aa736eb
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2021-01-07 04:41:52 +0000

    editing main file

--- src/SwimmingStudents/Main.java
+++ src/SwimmingStudents/Main.java
@@ -14,9 +14,14 @@ public class Main {
     String strRegister;
     int intRegistered = 10;
     int intMember;
+    String strFirstName;
+    String strLastName;
+    int intAge;
+
     while(blnOption == true) {
       System.out.println("Hi, welcome to swimming pool online orginizer");
-      System.out.println("Are you a: student or worker?");
+      System.out.println("Are you a student or worker?");
+      System.out.println("Type done if you are finished");
       strOption = keyboard.readLine();
       if(strOption.equalsIgnoreCase("student")) {
         System.out.println("Have you registered at our community center before?");
@@ -28,9 +33,47 @@ public class Main {
           if(intMember == 1) {
             Student one = new Student("John", "Lau", 5, 2, 10); 
             System.out.println(one);
+          }else if(intMember == 2) {
+            Student one = new Student("Tom", "Cruise", 5, 2, 13); 
+            System.out.println(one);
+          }else if(intMember == 3) {
+            Student one = new Student("Hayden", "Chung", 10, 1, 17); 
+            System.out.println(one);
+          }else if(intMember == 4) {
+            Student one = new Student("Timmy", "Smith", 6, 4, 12); 
+            System.out.println(one);
+          }else if(intMember == 5) {
+            Student one = new Student("Dom", "George", 7, 5, 14); 
+            System.out.println(one);
+          }else if(intMember == 6) {
+            Student one = new Student("Myers", "James", 7, 5, 13); 
+            System.out.println(one);
+          }else if(intMember == 7) {
+            Student one = new Student("Lebron", "James", 9, 3, 19); 
+            System.out.println(one);
+          }else if(intMember == 8) {
+            Student one = new Student("Jimmy", "Lau", 6, 4, 13); 
+            System.out.println(one);
+          }else if(intMember == 9) {
+            Student one = new Student("Bronny", "JR", 10, 1, 17); 
+            System.out.println(one);
+          }else if(intMember == 10) {
+            Student one = new Student("Stephen", "Curry", 9, 3, 18); 
+            System.out.println(one);
           }
+        }else if(strRegister.equalsIgnoreCase("no")) {
+          System.out.println("Since you have never registered in our swimming pool, you will start at level 1");
+          System.out.println("What is your first name?");
+          strFirstName = keyboard.readLine();
+          System.out.println("What is your last name?");
+          strLastName = keyboard.readLine();
+          System.out.println("What is your age?");
+          intAge = Integer.parseInt(keyboard.readLine());
+          Student one = new Student(strFirstName, strLastName, 1, 6, intAge);
+          System.out.println(one);
+          System.out.println("You will not be visable on the list until you attend the first lesson");
         }
-      }else if(strOption.equalsIgnoreCase("teacher")) {
+      }else if(strOption.equalsIgnoreCase("worker")) {
        
       }else if(strOption.equalsIgnoreCase("Done")) {
         System.out.println("Goodbye!");
