77130a7c91759c2090ffad622e614e9474dde22d
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2021-01-07 17:39:09 +0000

    create comments for people class

--- src/SwimmingStudents/People.java
+++ src/SwimmingStudents/People.java
@@ -1,14 +1,23 @@
 package SwimmingStudents;
 
 import SwimmingStudents.*;
-
+/**
+* super class
+*/
 public class People {
 
+  // Instance Variables
   private String strFirstName;
   private String strLastName;
   private int intLevel;
   private int intLaneNumber;
-
+  /**
+  * Constructor: Creates a new person Instance
+  * @param firstName - person's first name
+  * @param lastName - person's last Name
+  * @param Level - person's level in Swimming 
+  * @param intLane - person's lane number
+  */
   public People(String firstName, String lastName, int Level, int intLane) {
     this.strFirstName = firstName;
     this.strLastName = lastName;
@@ -16,18 +25,34 @@ public class People {
     this.intLaneNumber = intLane;
   }
 
+  /**
+  * Getter method for firstName
+  * @return first name
+  */
   public String getFirstName() {
     return strFirstName;
   }
 
+  /**
+  * Getter method for lastName
+  * @return last name
+  */
   public String getLastName() {
     return strLastName;
   }
-
+  
+  /**
+  * Getter method for Level
+  * @return level number
+  */
    public int getLevel() {
     return intLevel;
   }
 
+  /**
+  * Getter method for intLane
+  * @return lane number
+  */
   public int getLane() {
     return intLaneNumber;
   }
