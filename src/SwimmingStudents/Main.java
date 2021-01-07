406bd145dbe06376198de3bef5d8c0e2cb12a067
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2021-01-07 02:14:07 +0000

    Editing main

--- src/SwimmingStudents/Main.java
+++ src/SwimmingStudents/Main.java
@@ -13,33 +13,30 @@ public class Main {
     boolean blnOption = true;
     String strFirstName;
     String strLastName;
-    int intPassword;
-
-    System.out.println("Hi, welcome to swimming pool online orginizer");
-    System.out.println("Are you a: student, teacher, or supervisor");
-    strOption = keyboard.readLine();
     while(blnOption == true) {
+      System.out.println("Hi, welcome to swimming pool online orginizer");
+      System.out.println("Are you a: student, teacher, or supervisor");
+      strOption = keyboard.readLine();
       if(strOption.equalsIgnoreCase("student")) {
         System.out.println("What is your first name");
-        strFirstName = 
+        strFirstName = keyboard.readLine();
         System.out.println("What is your last name");
-        System.out.println("What is your password");
+        strLastName = keyboard.readLine();
+        
       }else if(strOption.equalsIgnoreCase("teacher")) {
         System.out.println("What is your fire name");
+        strFirstName = keyboard.readLine();
         System.out.println("What is your last name");
-        System.out.println("What is your ID?");
+        strLastName = keyboard.readLine();
       }else if(strOption.equalsIgnoreCase("supervisor")) {
         System.out.println("What is your name");
+        strFirstName = keyboard.readLine();
         System.out.println("What is your last name");
-        System.out.println("What is your ID?");
-      }else if(strOtpion.equals("Done")) {
+        strLastName = keyboard.readLine();
+      }else if(strOption.equalsIgnoreCase("Done")) {
         System.out.println("Goodbye!");
         blnOption = false;
       }
     }
-
-    
   }
- 
-
 }
--- src/SwimmingStudents/Supervisor.java
+++ src/SwimmingStudents/Supervisor.java
@@ -7,4 +7,5 @@ public class Supervisor {
 public Supervisor(String firstName, String lastName, double Pay) {
     super(firstName, lastName, Pay);
   }
+  
 }
